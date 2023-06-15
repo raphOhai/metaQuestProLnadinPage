f5b124ebb83da87c8fb50ac614058eb6f24fb937
raphaelohai5 <sameleno6@gmail.com>
2023-06-15 21:42:50 +0100

    ffff

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,41 +5,43 @@ import Cart from "../../assets/cart";
 import Search from "../../assets/search";
 const Header = () => {
   return (
-    <div className="fixed ">
-      <div className="stack3 maxWidth2 defaultPadding ">
-        <div className="headerBoxContent">
-          <div className="smallText1 logo flex2 center1 ">
-            <img  src="/oculus.webp" alt="" />
-            <p className="headingTex hideMobileFlex"> meta Quest</p>
-            <p className="headingTex hideMobileFlex">RayBan stores</p>
-          </div>
-
-          <div className=" flex2 center1">
-            <div>
-              <Cart />
+    <div className="fixed maxWidth2 ">
+      <div className=" defaultPadding">
+        <div className="stack3  ">
+          <div className="headerBoxContent">
+            <div className="smallText1 logo flex2 center1 ">
+              <img src="/oculus.webp" alt="" />
+              <p className="headingTex hideMobileFlex"> meta Quest</p>
+              <p className="headingTex hideMobileFlex">RayBan stores</p>
             </div>
-            <div>
-              <Search />
+
+            <div className=" flex2 center1">
+              <div>
+                <Cart />
+              </div>
+              <div>
+                <Search />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div style={{marginTop:"-1rem"}} className="headerBoxContent">
-          <div >
-            <p id="productText" className="boldText ">
-              Quest Pro
-            </p>
-          </div>
-
-          <div className="headerBoxContent flex2 center1">
-            <p id="product2" className="graySmallText hideMobileFlex">
-              overview
-            </p>
-            <p id="product2" className="smallText1 hideMobileFlex">
-              Tech Specs
-            </p>
+          <div style={{ marginTop: "-1rem" }} className="headerBoxContent">
             <div>
-              <button className="buynow">find a store</button>
+              <p id="productText" className="boldText ">
+                Quest Pro
+              </p>
+            </div>
+
+            <div className="headerBoxContent flex2 center1">
+              <p id="product2" className="graySmallText hideMobileFlex">
+                overview
+              </p>
+              <p id="product2" className="smallText1 hideMobileFlex">
+                Tech Specs
+              </p>
+              <div>
+                <button className="buynow">find a store</button>
+              </div>
             </div>
           </div>
         </div>
