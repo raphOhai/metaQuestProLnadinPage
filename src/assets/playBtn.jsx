f4bc6e7dd61ea47c2787ea42c0ddb782b34cf585
raphaelohai5 <sameleno6@gmail.com>
2023-06-16 20:08:48 +0100

    play btn fixed

--- src/assets/playBtn.jsx
+++ src/assets/playBtn.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const PlayBtn = () => {
   return (
     <svg
-      width="24"
-      height="24"
+      width="40"
+      height="40"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
